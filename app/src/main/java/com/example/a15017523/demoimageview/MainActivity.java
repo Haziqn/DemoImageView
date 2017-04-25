eb4d3cd176bbdf9a84d5b3b1139ff7594ef842c6
haziqn <hazinqhar20@gmail.com>
2017-04-25 14:59:46 +0800

    latest

--- app/src/main/java/com/example/a15017523/demoimageview/MainActivity.java
+++ app/src/main/java/com/example/a15017523/demoimageview/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.a15017523.demoimageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
